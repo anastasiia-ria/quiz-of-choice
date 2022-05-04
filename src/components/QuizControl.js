790a6b27e169bd9e9bbba43bbaccc2958d2b06c3
Liam Eller <liamthelastson@gmail.com>
2022-05-04 09:19:19 -0700

    Add progress with hooks
    Co-authored-by: Anastasiia Riabets <anastasiia-ria@users.noreply.github.com>

--- src/components/QuizControl.js
+++ src/components/QuizControl.js
@@ -3,15 +3,31 @@ import { connect } from 'react-redux';
 import PropTypes from "prop-types";
 import * as a from './../actions';
 import { withFirestore, isLoaded } from 'react-redux-firebase';
+import { useDispatch } from 'react-redux'
 
 // class QuizControl extends React.Component {
 // }
 
 const QuizControl = () => {
-  const quizList = [quizzes, setQuiz] = useState({});
-
+  const quizzes = [quizList, setQuiz] = useState({});
+  const selectedQuiz = [state, setSelectedQuiz] = useState({});
+  
   const handleAddingQuiz = (quiz) => {
     // logic for adding quiz to firebase
+    // should take the new quiz and use setQuiz function defined above
+  }
+
+  function handleClick() {
+    if (selectedQuiz.state != null) {
+      selectedQuiz.setSelectedQuiz({
+        selectedQuiz: null,
+        editing: false
+      });
+    } else {
+      const dispatch = useDispatch();
+      const action = a.toggleForm();
+      dispatch(action);
+    }
   }
   
   const auth = this.props.firebase.auth();
@@ -27,12 +43,29 @@ const QuizControl = () => {
     <React.Fragment>
       <h1 className="text-center">You Must Be Signed In To Access The Quizzes!</h1>
     </React.Fragment>
-    )}
+    )
+  }
   if ((isLoaded(auth)) && (auth.currentUser != null)) {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    currentlyVisibleState = <QuizList onTicketSelection={handleChangingSelectedTicket} />
+    buttonText = "Add Quiz";
     return (
-      
+      <React.Fragment>
+        {currentlyVisibleState}
+        <div className="flex justify-center">  
+          <button className="bg-yellow-500 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full mt-2" onClick={handleClick}>{buttonText}</button>
+        </div>
+      </React.Fragment>
     )
   }
 }
 
+
+const mapStateToProps = state => {
+  return {
+    formVisibleOnPage: state.formVisibleOnPage
+  }
+}
+QuizControl = connect(mapStateToProps)(QuizControl);
 export default withFirestore(QuizControl);
